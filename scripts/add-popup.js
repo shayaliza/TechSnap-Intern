fe78143ccf6788505d570f93cb9e6101b104a84b
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2023-09-24 02:08:41 +0530

    announcement page update

--- scripts/add-popup.js
+++ scripts/add-popup.js
@@ -1,64 +1,80 @@
-var add_btn = document.getElementsByClassName('add-btn')
-var add_popup = document.getElementsByClassName('add-popup')[0]
-var body = document.getElementsByClassName('body')[0]
-var navi = document.getElementsByClassName("navigation")[0]
-var cancel_btn1 = document.getElementsByClassName('cancel')[0]
-var cancel_icon1 = document.getElementsByClassName('cancel-icon')[0]
-var cancel_btn2 = document.getElementsByClassName('cancel')[1]
-var cancel_icon2 = document.getElementsByClassName('cancel-icon')[1]
-var panel = document.getElementsByClassName("side-panel-container")[0]
-var edit_btn = document.getElementsByClassName("edit-btn")[0]
-var edit_popup = document.getElementsByClassName("edit-popup")[0]
+document.addEventListener('DOMContentLoaded', function () {
 
-for(var i = 0; i < 2; i++)
-{
-    add_btn[i].addEventListener("click", () => {
-        add_popup.style.display = "block"
-        body.style.filter = "blur(10px)"
-        navi.style.filter = "blur(10px)"
-        panel.style.filter = "blur(10px)"
-        document.body.style.position = "fixed"
-    })
-}
+    // ============== announcement popup =================
 
-cancel_btn1.addEventListener("click", () => {
-    add_popup.style.display = "none"
-    body.style.filter = "none"
-    navi.style.filter = "none"
-    panel.style.filter = "none"
-    document.body.style.position = "relative"
-})
-cancel_icon1.addEventListener("click", () => {
-    add_popup.style.display = "none"
-    body.style.filter = "none"
-    navi.style.filter = "none"
-    panel.style.filter = "none"
-    document.body.style.position = "relative"
-})
+    const announcementModal = document.getElementById('announcementModal');
+    const openPopupBtn = document.getElementById('openAnnouncementPopup');
+    const closeModalBtn = document.getElementById('closeAnnouncementPopup');
+    const announcementHeadingInput = document.getElementById('announcementHeading');
+    const announcementDescriptionTextarea = document.getElementById('announcementDescription');
 
-cancel_btn2.addEventListener("click", () => {
-    edit_popup.style.display = "none"
-    body.style.filter = "none"
-    navi.style.filter = "none"
-    panel.style.filter = "none"
-    document.body.style.position = "relative"
-})
 
-cancel_icon2.addEventListener("click", () => {
-    edit_popup.style.display = "none"
-    body.style.filter = "none"
-    navi.style.filter = "none"
-    panel.style.filter = "none"
-    document.body.style.position = "relative"
-})
+    // Function to open the modal
+    function openAnouncementModal() {
+        announcementModal.style.display = 'block';
+    }
 
-edit_btn.addEventListener("click", () => {
-    edit_popup.style.display = "block"
-    body.style.filter = "blur(10px)"
-    navi.style.filter = "blur(10px)"
-    panel.style.filter = "blur(10px)"
-    document.body.style.position = "fixed"
-})
+    // Function to close the modal
+    function closeAnnouncementModal() {
+        announcementModal.style.display = 'none';
+    }
 
-document.getElementsByClassName('textarea')[0].defaultValue = "Announcement-1"
-document.getElementsByClassName('textarea')[1].defaultValue = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi blanditiis hic atque quisquam consequatur numquam illo tenetur nostrum quibusdam impedit eligendi doloribus ea ab fugit iure veritatis, debitis perferendis sint provident deserunt ex reiciendis omnis deleniti!"
+    // Event listener to open the modal when the button is clicked
+    openPopupBtn.addEventListener('click', openAnouncementModal);
+
+    // Event listener to close the modal when the close button is clicked
+    closeModalBtn.addEventListener('click', closeAnnouncementModal);
+
+    announcementHeadingInput.defaultValue='Announcement';
+    announcementDescriptionTextarea.defaultValue='Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem laborum eaque sequi, quod quam earum asperiores, quibusdam libero rem a placeat quis doloribus eligendi cum maiores iusto facere repellat. Sequi asperiores ducimus, quae nihil labore atque totam voluptatem vitae laborum quibusdam repellendus, eum, iure est dolor quasi neque sit doloremque.'
+    
+
+    //================link popup =============
+
+    const linkModal = document.getElementById('linkModal');
+    const addLinkBtn = document.getElementById('addLinkBtn');
+    const closeLinkModalBtn = document.getElementById('closeLinkModal');
+
+
+    // Function to open the modal
+    function openLinkModal() {
+        linkModal.style.display = 'block';
+    }
+
+    // Function to close the modal
+    function closeLinkModal() {
+        linkModal.style.display = 'none';
+    }
+
+    // Event listener to open the modal when the button is clicked
+    addLinkBtn.addEventListener('click', openLinkModal);
+
+    // Event listener to close the modal when the close button is clicked
+    closeLinkModalBtn.addEventListener('click', closeLinkModal);
+
+
+
+    // =============== upload popup ===========
+
+    const fileModal = document.getElementById('fileModal');
+    const uploadFileBtn = document.getElementById('uploadFileBtn');
+    const closefileBtn = document.getElementById('closefileBtn');
+
+
+    // Function to open the modal
+    function openfileModal() {
+        fileModal.style.display = 'block';
+    }
+
+    // Function to close the modal
+    function closefileModal() {
+        fileModal.style.display = 'none';
+    }
+
+    // Event listener to open the modal when the button is clicked
+    uploadFileBtn.addEventListener('click', openfileModal);
+
+    // Event listener to close the modal when the close button is clicked
+    closefileBtn.addEventListener('click', closefileModal);
+
+});
